279f2582c07bca769ca9f10da217621de253be29
gahag <gabriel.s.b@live.com>
2022-03-06 14:23:32 -0300

    Implement std.export

--- src/runtime/lib/env.rs
+++ src/runtime/lib/env.rs
@@ -1,5 +1,7 @@
 use gc::{Finalize, Trace};
 
+use crate::runtime::value::Error;
+
 use super::{
 	CallContext,
 	RustFun,
@@ -10,6 +12,7 @@ use super::{
 
 
 inventory::submit! { RustFun::from(Env) }
+inventory::submit! { RustFun::from(Export) }
 
 #[derive(Trace, Finalize)]
 struct Env;
@@ -28,6 +31,41 @@ impl NativeFun for Env {
 
 			[ other ] => Err(Panic::type_error(other.copy(), "string", context.pos)),
 			args => Err(Panic::invalid_args(args.len() as u32, 1, context.pos))
+		}
+	}
+}
+
+#[derive(Trace, Finalize)]
+struct Export;
+
+impl NativeFun for Export {
+	fn name(&self) -> &'static str { "std.export" }
+
+	fn call(&self, context: CallContext) -> Result<Value, Panic> {
+		match context.args() {
+			[ k @ Value::String(ref key), v @ Value::String(ref value) ] => {
+				let ret = match () {
+					() if key.contains(b'=') || key.contains(b'\0') => {
+						Error::new("invalid export key".into(), k.copy()).into()
+					}
+
+					() if value.contains(b'\0') => {
+						Error::new("invalid export value".into(), v.copy()).into()
+					}
+
+					() => {
+						std::env::set_var(key, value);
+						Value::default()
+					},
+				};
+
+				Ok(ret)
+			},
+
+			[ Value::String(_), other ] => Err(Panic::type_error(other.copy(), "string", context.pos)),
+			[ other, _ ] => Err(Panic::type_error(other.copy(), "string", context.pos)),
+
+			args => Err(Panic::invalid_args(args.len() as u32, 2, context.pos)),
 		}
 	}
 }
